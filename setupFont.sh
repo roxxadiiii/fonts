9e6da027fdeebb4b0dfdc582446e11d8d13e7f59
roxxadiiii <itsmeadityakumararya@gmail.com>
2024-10-28 18:19:04 +0530

    script fix

--- setupFont.sh
+++ setupFont.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Copying fonts to system dir..."
-sudo cp -r *.ttf /usr/share/fonts/custom-fonts/
+sudo cp -r *.ttf /usr/share/fonts/
 echo "copying done"
 
 echo "clearing cache.........."
