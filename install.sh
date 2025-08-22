4ae2b4779314c1f3a4cff468a3a50f76f79452f6
roxxadiiii <itsmeadityakumararya@gmail.com>
2025-08-22 11:38:17 +0530

    MapleMono fonts added

--- install.sh
+++ install.sh
@@ -8,11 +8,16 @@ sudo cp -r GeistMono/ /usr/share/fonts/
 echo "GeistMono fonts copied"
 
 
+echo "copying FantasqueSansMono fonts"
+sudo cp -r FantasqueSansMono/ /usr/share/fonts/
+
+echo "FantasqueSansMono fonts copied"
 
 
+echo "copying MapleMono"
+sudo cp -r MapleMono/ /usr/share/fonts/
 
-
-
+echo "MapleMono fonts copied"
 # reloding font config
 
 sudo fc-cache -f -v
